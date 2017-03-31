bf30282580d0e9168bae8ad7453e3f867f4155c5
rmit-s3587683-swapnil-guha <s3587683@student.rmit.edu.au>
2017-03-31 21:27:26 +1100

    Commit Check

---
+++ src/Ozlympic.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author my
+ *
+ */
+public class Ozlympic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
